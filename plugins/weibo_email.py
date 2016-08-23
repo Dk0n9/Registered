4bb6168dc4c4014b47d0a041c36e3a0fffebbdc2
dk0n9 <15874911@qq.com>
2016-08-23 23:57:22 +0800

    添加微博插件

---
+++ plugins/weibo_email.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+from Registered.common import base
+
+
+class Plugin(base.BASE):
+
+    __name__ = 'weibo_email'  # 只能使用字母、数字、英文下划线命名, 字母开头
+    __title__ = '微博'
+    __url__ = 'http://www.weibo.com/'
+
+    def register(self, target):
+        self.url = 'http://weibo.com/signup/v5/formcheck'
+        self.method = 'get'
+        self.settings = {
+            'params': {
+                'type': 'email',
+                'value': target
+            },
+            'headers': {
+                'user-agent': self.getRandomAgent(),
+                'referer': 'http://weibo.com/signup/signup.php'
+            }
+        }
+        self.resultType = 'json'
+        self.resultValue = 'code=100000'
+
+
+if __name__ == '__main__':
+    test = Plugin()
+    test.register('xxx')
+    print test.verify()
