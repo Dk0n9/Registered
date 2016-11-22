41d5837059f04d9ac1855c880c617161ef3236aa
dk0n9 <15874911@qq.com>
2016-11-23 00:23:40 +0800

    新增一个打印异常信息的方法

--- common/exceptions.py
+++ common/exceptions.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import sys
+
 
 class DBConnectionError(Exception):
     pass
@@ -7,3 +9,11 @@ class DBConnectionError(Exception):
 
 class DBAuthenticatedError(Exception):
     pass
+
+
+def printException():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    print '[!] {filename} LINE {line} Error: {error}'.format(filename=filename, line=lineno, error=exc_obj)
